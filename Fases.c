13528e490f5305ef9c13817a620da7f68ef25c39
Lourival Sousa <lourivalsousadev@gmail.com>
2022-03-09 23:31:59 -0300

    Variaveis principais  da fase 1 definidas

--- Fases.c
+++ Fases.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Tabuleiro.c"
 #include "Game.c"
 
-void definirComandosFaseUm(char (*ptrComandos)[4]){
+void comandosFaseUm(char (*ptrComandos)[4]){
     int i;
 
     for(i=0;i<4;i++){
@@ -10,32 +11,34 @@ void definirComandosFaseUm(char (*ptrComandos)[4]){
             ptrComandos[1][i] = '0';
         }
     }
+    //Comando 1
     ptrComandos[0][0] = 'F';
     ptrComandos[0][1] = 'F';
     ptrComandos[0][2] = 'F';
-
+    //Comando 2
     ptrComandos[1][0] = 'E';
     ptrComandos[1][1] = 'F';
-
+    // Comando 3
     ptrComandos[2][0] = 'D';
     ptrComandos[2][1] = 'F';
-
+    // Comando 4
     ptrComandos[3][0] = 'F';
     ptrComandos[3][1] = 'E';
-  
 }
 
-void primeiraFase(){
-    // definir a matriz de comandos
+int FaseUm(){
+    char matriz[8][8],comandos[4][4];
+    char (*ptrMatriz)[8] = matriz;
+    char (*ptrComandos)[4] = comandos;
+    initBoard(ptrMatriz);
+    comandosFaseUm(ptrComandos);
+}
 
-    // Inicializando as variaveis
-    char matriz[8][8],direcao,comando;
-    char (*ptrMatriz)[8] = &matriz;
-    char *ptrDirecao = &direcao;
-    int posicao[2],x,y;
-    int *ptrPosicao = &posicao;
-    //Comandos
-    char comandos[4][4], *ptrComandos;
-    (*ptrComandos)[4] = &comandos;
 
+int main (){
+    // char matriz[8][8],comandos[4][4];
+    // char (*ptrMatriz)[8] = matriz;
+    // char (*ptrComandos)[4] = comandos;
+    // initBoard(ptrMatriz);
+    return 0;
 }
